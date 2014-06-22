ed7436d804f6844e3b044b66ee246ae771ed367b
jgerardsimcock <justin_simcock@yahoo.com>
2014-06-21 15:48:40 -1000

    -Challenge_2 Working

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,22 @@
-exports.sumOfEvenFibonacciNumbers = function(n){
-  var sum = 0;
-  // do your work here
+ exports.sumOfEvenFibonacciNumbers = function(n){
   
-  return sum;
-};
+  var sum = 0;
+  var fib1 = 1;
+  var fib2 = 1;
+  var fibSum = 2;
+
+for(var i = 0; fibSum < n; i++ ) {
+
+  // while (fibSum < n) {
+    if ((fibSum % 2) === 0) {
+      sum += fibSum;
+      }
+    fib2 = fib1;
+    fib1 = fibSum;
+    fibSum += fib2;
+  }
+    return sum;
+ 
+ console.log(sumOfEvenFibonacciNumbers(10));
+  };
+  
